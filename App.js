6a5c39fd77a005c4a445f220f718f592f03cd77d
Da-Rk0101 <ahmedshetat01@gmail.com>
2024-05-08 20:00:29 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 
 import Navbar from './components/Navbar';
+import ProductsList from './components/productsList';
 import Slider from './components/slider';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Slider/>
+      <ProductsList/>
     </div>
   );
 }
